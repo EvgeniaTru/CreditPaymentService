e857d2e9dc6bd245da718b983b38d143fb40cc46
Evgeniia Trusheva <evgenia.tru@gmail.com>
2020-06-19 02:42:30 +0400

    CreditPaymentService app created

---
+++ CreditPaymentService.java
@@ -0,0 +1,9 @@
+public class CreditPaymentService {
+    public int calculate (long amount, int period, float percent) {
+        float monthlyPercent = percent/12/100F;
+        float index = monthlyPercent+1;
+        float power = (float) Math.pow(index,period);
+        int payment = (int) ((power/(power-1))*monthlyPercent*amount);
+        return payment;
+    }
+}
