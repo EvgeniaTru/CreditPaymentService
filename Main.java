4c77caa87538117dfdbe73e9e9bd65ae23a3b37f
Evgeniia Trusheva <evgenia.tru@gmail.com>
2020-06-19 11:49:57 +0400

    Code reformated

--- Main.java
+++ Main.java
@@ -1,11 +1,11 @@
 public class Main {
     public static void main(String[] args) {
         CreditPaymentService service = new CreditPaymentService();
-        int payment = service.calculate (1_000_000,12,9.99F);
+        int payment = service.calculate(1_000_000, 12, 9.99F);
         System.out.println(payment);
-        int payment2 = service.calculate (1_000_000,24,9.99F);
+        int payment2 = service.calculate(1_000_000, 24, 9.99F);
         System.out.println(payment2);
-        int payment3 = service.calculate (1_000_000,36,9.99F);
+        int payment3 = service.calculate(1_000_000, 36, 9.99F);
         System.out.println(payment3);
     }
 }
